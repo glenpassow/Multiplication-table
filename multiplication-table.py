98dfae33020c5adea52082c9d412e1673ebe20a6
glenpassow <glenpassow@hanovernorwichschools.org>
2016-02-08 14:20:09 -0500

    Updated from Brython Server: February 8, 2016 2:20:09 PM EST

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Glen Passow
+Credit: Hagin
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
@@ -21,3 +21,5 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+w = input("Width of multiplication table: ")
+h = input("Height of multiplication table: ")
